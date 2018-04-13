b1dcbdc696abac9a695be1b56a1f464b42979c38
inddiara <indiara06d@gmail.com>
2018-04-13 16:28:19 -0400

    Iata attribute validation

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,5 +1,5 @@
 class Airport < ApplicationRecord
-  validates :iata, presence: true
+  validates :iata, presence: true, length: {maximum: 3}
   validates :name, presence: true
   validates :boarding_fee, presence: true
 end
