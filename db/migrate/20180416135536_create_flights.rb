c97baf9bb083f276c0d8cfa7d243dfaaac38e63f
Weyler Almeida <weyler180@gmail.com>
2018-04-16 13:30:46 -0300

    #10 - Adjusting references of flight
    Signed-off-by: Weyler Almeida <weyler180@gmail.com>
    Signed-off-by: Thiago Pereira <thiagoflames@gmail.com>

--- db/migrate/20180416135536_create_flights.rb
+++ db/migrate/20180416135536_create_flights.rb
@@ -1,5 +1,6 @@
 class CreateFlights < ActiveRecord::Migration[5.1]
   def change
+
     create_table :flights, id: :uuid do |t|
       t.string :flight_number
       t.integer :departure
@@ -7,8 +8,16 @@ class CreateFlights < ActiveRecord::Migration[5.1]
       t.integer :duration
       t.datetime :created_at
       t.datetime :updated_at
+      t.references :airline, foreign_key: true
+      t.references :aircraft, foreign_key: true
 
       t.timestamps
     end
+
+    add_reference :flights, :origin, type: :uuid, references: :airports, index: true, after: :id
+    add_foreign_key :flights, :airports, column: :origin_id
+    add_reference :flights, :destination, type: :uuid, references: :airports, index: true, after: :origin_id
+    add_foreign_key :flights, :airports, column: :destination_id
+
   end
 end
