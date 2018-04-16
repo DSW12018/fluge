fc6e579b34b01adbb4800dc0de45431b4748bb1d
Weyler Almeida <weyler180@gmail.com>
2018-04-16 13:34:05 -0300

    #10 - Adding tests of flight
    Signed-off-by: Weyler Almeida <weyler180@gmail.com>
    Signed-off-by: Thiago Pereira <thiagoflames@gmail.com>

--- spec/factories/airports.rb
+++ spec/factories/airports.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :airport do
-    iata "String"
+    iata "Str"
     name "NameString"
     boarding_fee "20"
   end
--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not create Flight when flight_number is blank" do
+    begin
+      FactoryBot.create :flight, flight_number: ''
+    rescue ActiveRecord::RecordInvalid => invalid
+      errors = invalid.record.errors
+      expect(errors.include?(:flight_number)).to be(true)
+    end
+  end
+
+  it "should not create Flight when departure is a string" do
+    begin
+      FactoryBot.create :flight, departure: "string"
+    rescue ActiveRecord::RecordInvalid => invalid
+      errors = invalid.record.errors
+      expect(errors.include?(:departure)).to be(true)
+    end
+  end
+
+  it "should not create Flight when arrival is a string" do
+    begin
+      FactoryBot.create :flight, arrival: "string"
+    rescue ActiveRecord::RecordInvalid => invalid
+      errors = invalid.record.errors
+      expect(errors.include?(:arrival)).to be(true)
+    end
+  end
+
+  it "should not create Flight when duration is a string" do
+    begin
+      FactoryBot.create :flight, duration: "string"
+    rescue ActiveRecord::RecordInvalid => invalid
+      errors = invalid.record.errors
+      expect(errors.include?(:duration)).to be(true)
+    end
+  end
 end
