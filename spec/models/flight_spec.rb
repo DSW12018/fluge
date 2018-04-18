6a8cf6e5d600b0e6e67da5ffa68d39b5efa94f5d
Kairon Velozo <kairon333@gmail.com>
2018-04-18 11:40:00 -0300

    #10 - Creating tests for validating association fields of the Flight model.

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Flight, type: :model do
     end
   end
 
-  it "should not create Flight when departure is a string" do
+  it "should not create Flight when departure is not a number" do
     begin
       FactoryBot.create :flight, departure: "string"
     rescue ActiveRecord::RecordInvalid => invalid
@@ -19,7 +19,7 @@ RSpec.describe Flight, type: :model do
     end
   end
 
-  it "should not create Flight when arrival is a string" do
+  it "should not create Flight when arrival is not a number" do
     begin
       FactoryBot.create :flight, arrival: "string"
     rescue ActiveRecord::RecordInvalid => invalid
@@ -28,12 +28,48 @@ RSpec.describe Flight, type: :model do
     end
   end
 
-  it "should not create Flight when duration is a string" do
+  it "should not create Flight when duration not a number" do
     begin
       FactoryBot.create :flight, duration: "string"
     rescue ActiveRecord::RecordInvalid => invalid
       errors = invalid.record.errors
       expect(errors.include?(:duration)).to be(true)
+    end
+  end
+
+  it "should not create Flight when airline is not an airline" do
+    begin
+      FactoryBot.create :flight, airline: nil
+    rescue ActiveRecord::RecordInvalid => invalid
+      errors = invalid.record.errors
+      expect(errors.include?(:airline)).to be(true)
+    end
+  end
+
+  it "should not create Flight when aircraft is not an aircraft" do
+    begin
+      FactoryBot.create :flight, aircraft: nil
+    rescue ActiveRecord::RecordInvalid => invalid
+      errors = invalid.record.errors
+      expect(errors.include?(:aircraft)).to be(true)
+    end
+  end
+
+  it "should not create Flight when origin is not an airport" do
+    begin
+      FactoryBot.create :flight, origin: nil
+    rescue ActiveRecord::RecordInvalid => invalid
+      errors = invalid.record.errors
+      expect(errors.include?(:origin)).to be(true)
+    end
+  end
+
+  it "should not create Flight when destination is not an airport" do
+    begin
+      FactoryBot.create :flight, destination: nil
+    rescue ActiveRecord::RecordInvalid => invalid
+      errors = invalid.record.errors
+      expect(errors.include?(:destination)).to be(true)
     end
   end
 end
