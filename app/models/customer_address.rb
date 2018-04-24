6a0dea88a564bace421a0a710bbe0bdb3064e1c4
Breno Mariz <mrzbeo@gmail.com>
2018-04-24 01:28:26 -0300

    Creating tests for customer_address
    Signed-off-by: Breno Mariz <mrzbeo@gmail.com>
    Signed-off-by: Vinicius Oliveira <vinifladf@gmail.com>

--- app/models/customer_address.rb
+++ app/models/customer_address.rb
@@ -1,3 +1,13 @@
 class CustomerAddress < ApplicationRecord
   belongs_to :federated_unit
+
+  validates :zip_code, presence: true
+  validates :zip_code, format: { with: /\A[0-9\s]+\z/i, message: "can only contain numbers." }
+  validates :address, presence: true
+  validates :number, presence: true
+  validates :number, format: { with: /\A[0-9\s]+\z/i, message: "can only contain numbers." }
+  validates :complement, presence: true
+  validates :neighborhood, presence: true
+  validates :city, presence: true
+  validates :federated_unit, presence: true
 end
---
+++ spec/factories/customer_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :customer_address do
+    zip_code "4321"
+    address "MyString"
+    number "1234"
+    complement "MyString"
+    neighborhood "MyString"
+    city "MyString"
+    federated_unit
+  end
+end
